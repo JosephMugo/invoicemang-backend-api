2951eb369bc4fedf9e34d2ea75b173879e04a817
JosephMugo <joemu18@gmail.com>
2021-07-23 13:28:38 -0500

    initial commit

---
+++ src/main/java/com/invoicemang/invoicemangbackendapi/InvoicemangBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.invoicemang.invoicemangbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicemangBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicemangBackendApiApplication.class, args);
+	}
+
+}
