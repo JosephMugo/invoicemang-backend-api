66dec2482746dacf64cd66132ff448241462e5cf
JosephMugo <joemu18@gmail.com>
2021-12-07 22:53:35 -0600

    use response entity instead of pojo

--- src/main/java/com/invoicemang/invoicemangbackendapi/controller/PurchaseController.java
+++ src/main/java/com/invoicemang/invoicemangbackendapi/controller/PurchaseController.java
@@ -3,6 +3,8 @@ package com.invoicemang.invoicemangbackendapi.controller;
 import com.invoicemang.invoicemangbackendapi.model.Purchase;
 import com.invoicemang.invoicemangbackendapi.service.PurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,8 @@ public class PurchaseController {
     private PurchaseService purchaseService;
 
     @GetMapping
-    public List<Purchase> getPurchases() {
-        return purchaseService.getPurchases();
+    public ResponseEntity<List<Purchase>> getPurchases() {
+        List<Purchase> purchases = purchaseService.getPurchases();
+        return new ResponseEntity<>(purchases, HttpStatus.OK);
     }
 }
